86c497b93bc3115cc8c891d16711c5a7a4f11fe8
Doug Burks <doug.burks@gmail.com>
2019-03-06 15:01:22 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -28,9 +28,9 @@ latex_documents = [
 ]
 
 # Trying to get latex to create PDF in 7.5x9.25 format
-#latex_elements = {
-#    'preamble' : '\\usepackage[papersize={7.5in,9.25in}]{geometry}',
-#}
+latex_elements = {
+    'preamble' : '\usepackage[papersize={7.5in,9.25in}]{geometry}',
+}
 
 
 
