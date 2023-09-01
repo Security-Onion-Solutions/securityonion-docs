ab8260ad0dd18b0d6039dbe28d710b814ebe143b
Doug Burks <doug.burks@securityonionsolutions.com>
2023-09-01 13:00:06 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -31,6 +31,21 @@ latex_documents = [
    u'', 'manual'),
 ]
 
+extensions = [
+    'notfound.extension'
+]
+
+# sphinx-notfound-page
+# https://github.com/readthedocs/sphinx-notfound-page
+notfound_context = {
+    'title': 'Page Not Found',
+    'body': '''
+<h1>Page Not Found</h1>
+<p>Sorry, we couldn't find that page.</p>
+<p>Try using the search box or go to the homepage.</p>
+''',
+}
+
 ###########################################################################
 #          auto-created readthedocs.org specific configuration            #
 ###########################################################################
