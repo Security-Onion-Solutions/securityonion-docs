bafb56d7f03b3c40cdaec0981a3f8f1269356538
Jason Ertel <jason.ertel@securityonionsolutions.com>
2023-10-30 15:21:09 -0400

    fix logic

--- _static/theme_overrides.js
+++ _static/theme_overrides.js
@@ -5,7 +5,7 @@ View the <a href="https://docs.securityonion.net/">latest documentation</a>.
 `;
 
 function show_banner() {
-    if (!READTHEDOCS_DATA['version'] != '2.4') return;
+    if (READTHEDOCS_DATA['version'] == '2.4') return;
 
     const banner = document.createElement('div');
     banner.className = "so-banner";
