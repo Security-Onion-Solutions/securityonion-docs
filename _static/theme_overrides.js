90f275e67b3fc26c8d47002ce1f047f7e194e49c
Doug Burks <doug.burks@securityonionsolutions.com>
2024-04-25 16:21:49 -0400

    update theme_overrides.js

--- _static/theme_overrides.js
+++ _static/theme_overrides.js
@@ -5,7 +5,7 @@ View the <a href="https://docs.securityonion.net/">documentation for the latest 
 `;
 
 function show_banner() {
-    if (READTHEDOCS_DATA['version'] == '2.4') return;
+    if (READTHEDOCS_DATA['version'] != 'dev') return;
 
     const banner = document.createElement('div');
     banner.className = "so-banner";
