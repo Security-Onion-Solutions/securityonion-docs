c354c1ccdb9dfd3b3ef589eb644e87539cf8669d
reyesj2 <94730068+reyesj2@users.noreply.github.com>
2025-11-06 16:16:44 -0600

    clean build dir between builds

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ source .venv/bin/activate
 python -m pip install pip
 pip install -r requirements.txt
 pip install sphinx-autobuild
-sphinx-autobuild --port 4444 -b html -d $BUILDDIR/doctrees -T -j auto -W --watch . . $BUILDDIR/html
+sphinx-autobuild --port 4444 -b html -d $BUILDDIR/doctrees -T -j auto -W --pre-build "rm -rf $BUILDDIR/html/_static" --watch . . $BUILDDIR/html
